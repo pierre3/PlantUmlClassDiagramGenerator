172e61272bf731b73d2266b0224a30fe7e6c15e4
Rian Wouters <rian.wouters@lighting.com>
2019-02-08 22:23:49 +0100

    abstract from inheritance relations

---
+++ src/PlantUmlClassDiagramGenerator.Library/Relationship.cs
@@ -0,0 +1,25 @@
+﻿namespace PlantUmlClassDiagramGenerator.Library
+{
+    public abstract class Relationship
+    {
+        protected TypeNameText _baseTypeName;
+        protected TypeNameText _subTypeName;
+        protected string _baseLabel;
+        protected string _subLabel;
+        private string _symbol;
+
+        public Relationship(TypeNameText baseTypeName, TypeNameText subTypeName, string symbol, string baseLabel, string subLabel)
+        {
+            _baseTypeName = baseTypeName;
+            _subTypeName = subTypeName;
+            _symbol = symbol;
+            _baseLabel = string.IsNullOrWhiteSpace(baseLabel) ? "" : $" \"{baseLabel}\"";
+            _subLabel = string.IsNullOrWhiteSpace(subLabel) ? "" : $" \"{subLabel}\"";
+        }
+
+        public override string ToString()
+        {
+            return $"{_baseTypeName.Identifier}{_baseLabel} {_symbol} {_subTypeName.Identifier}";
+        }
+    }
+}
