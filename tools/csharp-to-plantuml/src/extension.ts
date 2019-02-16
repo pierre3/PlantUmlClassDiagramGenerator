70211f9de35886a75e781d8e6bd3c3ff9b2bc0b3
pierre3 <pierre3@outlook.com>
2019-02-16 15:21:21 +0900

    Add vscode extension

--- tools/csharp-to-plantuml/src/extension.ts
+++ tools/csharp-to-plantuml/src/extension.ts
@@ -1,6 +1,10 @@
+'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
+import { join } from 'path';
+import { isUndefined } from 'util';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,20 +12,34 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "csharp-to-plantuml" is now active!');
+	console.log('Congratulations, your extension "csharp-to-plantuml" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('csharp2plantuml.classDiagram', () => {
 		// The code you place here will be executed every time your command is executed
+		const outputchannel = vscode.window.createOutputChannel("CSharp to PlantUML");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const tool = join(context.extensionPath, 'lib', 'PlantumlClassDiagramGenerator', 'PlantUmlClassDiagramGenerator.dll');
+
+		const input = vscode.workspace.rootPath as string;
+		if(isUndefined(input)){
+			console.log("Open folder or workspace.");
+			return;
+		}
+		const output = join(input, 'diagram');
+		exec(`dotnet "${tool}" "${input}" "${output}" -dir -public`, (error, stdout, stderror) => {
+			outputchannel.appendLine(stdout);
+			outputchannel.appendLine(stderror);
+			if (error) {
+				console.log(error);
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
