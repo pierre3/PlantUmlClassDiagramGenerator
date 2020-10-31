fe3a0b75869083271500c9e440703ec75d1933fa
KOGA Mitsuhiro <shiena.jp@gmail.com>
2020-10-31 23:18:29 +0900

    add test for curly brackets

---
+++ test/PlantUmlClassDiagramGeneratorTest/testData/CurlyBrackets.cs
@@ -0,0 +1,22 @@
+namespace PlantUmlClassDiagramGeneratorTest
+{
+    public class CurlyBrackets
+    {
+        public string openingBracket = @"
+{
+";
+        public string openingBrackets = @"
+{{
+";
+        public string closingBracket = @"
+}
+";
+        public string closingBrackets = @"
+}}
+";
+        public string bothBrackets = @"
+{{
+}}
+";
+    }
+}
